475b16249341100dfe4985617c628cd150841fc7
platonev <platonewang@gmail.com>
2016-08-03 10:40:36 +0800

    format code

--- include/usb_cam/usb_cam.h
+++ include/usb_cam/usb_cam.h
@@ -48,6 +48,7 @@ extern "C"
 
 // legacy reasons
 #include <libavcodec/version.h>
+
 #if LIBAVCODEC_VERSION_MAJOR < 55
 #define AV_CODEC_ID_MJPEG CODEC_ID_MJPEG
 #endif
@@ -57,99 +58,114 @@ extern "C"
 
 #include <sensor_msgs/Image.h>
 
-namespace usb_cam {
+namespace usb_cam{
 
-class UsbCam {
- public:
-  typedef enum
-  {
-    IO_METHOD_READ, IO_METHOD_MMAP, IO_METHOD_USERPTR, IO_METHOD_UNKNOWN,
-  } io_method;
+    class UsbCam{
+      public:
+        typedef enum{
+            IO_METHOD_READ, IO_METHOD_MMAP, IO_METHOD_USERPTR, IO_METHOD_UNKNOWN,
+        } io_method;
 
-  typedef enum
-  {
-    PIXEL_FORMAT_YUYV, PIXEL_FORMAT_UYVY, PIXEL_FORMAT_MJPEG, PIXEL_FORMAT_YUVMONO10, PIXEL_FORMAT_RGB24, PIXEL_FORMAT_GREY, PIXEL_FORMAT_UNKNOWN
-  } pixel_format;
+        typedef enum{
+            PIXEL_FORMAT_YUYV,
+            PIXEL_FORMAT_UYVY,
+            PIXEL_FORMAT_MJPEG,
+            PIXEL_FORMAT_YUVMONO10,
+            PIXEL_FORMAT_RGB24,
+            PIXEL_FORMAT_GREY,
+            PIXEL_FORMAT_UNKNOWN
+        } pixel_format;
 
-  UsbCam();
-  ~UsbCam();
+        UsbCam();
 
-  // start camera
-  void start(const std::string& dev, io_method io, pixel_format pf,
-		    int image_width, int image_height, int framerate);
-  // shutdown camera
-  void shutdown(void);
+        ~UsbCam();
 
-  // grabs a new image from the camera
-  void grab_image(sensor_msgs::Image* image);
+        // start camera
+        void start(const std::string& dev, io_method io, pixel_format pf,
+                   int image_width, int image_height, int framerate);
 
-  // enables/disable auto focus
-  void set_auto_focus(int value);
+        // shutdown camera
+        void shutdown(void);
 
-  // Set video device parameters
-  void set_v4l_parameter(const std::string& param, int value);
-  void set_v4l_parameter(const std::string& param, const std::string& value);
+        // grabs a new image from the camera
+        void grab_image(sensor_msgs::Image* image);
 
-  static io_method io_method_from_string(const std::string& str);
-  static pixel_format pixel_format_from_string(const std::string& str);
+        // enables/disable auto focus
+        void set_auto_focus(int value);
 
-  void stop_capturing(void);
-  void start_capturing(void);
-  bool is_capturing();
+        // Set video device parameters
+        void set_v4l_parameter(const std::string& param, int value);
 
- private:
-  typedef struct
-  {
-    int width;
-    int height;
-    int bytes_per_pixel;
-    int image_size;
-    char *image;
-    int is_new;
-  } camera_image_t;
+        void set_v4l_parameter(const std::string& param, const std::string& value);
 
-  struct buffer
-  {
-    void * start;
-    size_t length;
-  };
+        static io_method io_method_from_string(const std::string& str);
 
+        static pixel_format pixel_format_from_string(const std::string& str);
 
-  int init_mjpeg_decoder(int image_width, int image_height);
-  void mjpeg2rgb(char *MJPEG, int len, char *RGB, int NumPixels);
-  void process_image(const void * src, int len, camera_image_t *dest);
-  int read_frame();
-  void uninit_device(void);
-  void init_read(unsigned int buffer_size);
-  void init_mmap(void);
-  void init_userp(unsigned int buffer_size);
-  void init_device(int image_width, int image_height, int framerate);
-  void close_device(void);
-  void open_device(void);
-  void grab_image();
-  bool is_capturing_;
+        void stop_capturing(void);
 
+        void start_capturing(void);
 
-  std::string camera_dev_;
-  unsigned int pixelformat_;
-  bool monochrome_;
-  io_method io_;
-  int fd_;
-  buffer * buffers_;
-  unsigned int n_buffers_;
-  AVFrame *avframe_camera_;
-  AVFrame *avframe_rgb_;
-  AVCodec *avcodec_;
-  AVDictionary *avoptions_;
-  AVCodecContext *avcodec_context_;
-  int avframe_camera_size_;
-  int avframe_rgb_size_;
-  struct SwsContext *video_sws_;
-  camera_image_t *image_;
+        bool is_capturing();
 
-};
+      private:
+        typedef struct{
+            int width;
+            int height;
+            int bytes_per_pixel;
+            int image_size;
+            char* image;
+            int is_new;
+        } camera_image_t;
 
+        struct buffer{
+            void* start;
+            size_t length;
+        };
+
+        int init_mjpeg_decoder(int image_width, int image_height);
+
+        void mjpeg2rgb(char* MJPEG, int len, char* RGB, int NumPixels);
+
+        void process_image(const void* src, int len, camera_image_t* dest);
+
+        int read_frame();
+
+        void uninit_device(void);
+
+        void init_read(unsigned int buffer_size);
+
+        void init_mmap(void);
+
+        void init_userp(unsigned int buffer_size);
+
+        void init_device(int image_width, int image_height, int framerate);
+
+        void close_device(void);
+
+        void open_device(void);
+
+        void grab_image();
+
+        bool is_capturing_;
+
+        std::string camera_dev_;
+        unsigned int pixelformat_;
+        bool monochrome_;
+        io_method io_;
+        int fd_;
+        buffer* buffers_;
+        unsigned int n_buffers_;
+        AVFrame* avframe_camera_;
+        AVFrame* avframe_rgb_;
+        AVCodec* avcodec_;
+        AVDictionary* avoptions_;
+        AVCodecContext* avcodec_context_;
+        int avframe_camera_size_;
+        int avframe_rgb_size_;
+        struct SwsContext* video_sws_;
+        camera_image_t* image_;
+    };
 }
 
 #endif
-
